224ed221456a313a95cc09821353a9c29e72e145
gfilaci <gianluca.filaci@gmail.com>
2017-07-27 14:57:46 +0100

    specialisations of initialisetwist moved in cc

--- lib/qcd/action/gauge/twistmatrices_pt.h
+++ lib/qcd/action/gauge/twistmatrices_pt.h
@@ -106,68 +106,6 @@ public:
     
 };
 
-
-// specialisation for SU(2) twist matrices
-template<>
-void twistmatrices<2>::initialisetwist(){
-    Complex im(0.,1.);
-    
-    identity()()()(0,0) = 1.;
-    identity()()()(0,1) = 0.;
-    identity()()()(1,0) = 0.;
-    identity()()()(1,1) = 1.;
-    
-    omega1()()()(0,0) = -im;
-    omega1()()()(0,1) = 0.;
-    omega1()()()(1,0) = 0.;
-    omega1()()()(1,1) = im;
-    
-    omega2()()()(0,0) = 0.;
-    omega2()()()(0,1) = 1.;
-    omega2()()()(1,0) = -1.;
-    omega2()()()(1,1) = 0.;
-}
-
-// specialisation for SU(3) twist matrices
-template<>
-void twistmatrices<3>::initialisetwist(){
-    Complex im(0.,1.);
-    double tmp = 2. * M_PI / 3.;
-    double tmpcos = std::cos(tmp);
-    double tmpsin = std::sin(tmp);
-    
-    identity()()()(0,0) = 1.;
-    identity()()()(0,1) = 0.;
-    identity()()()(0,2) = 0.;
-    identity()()()(1,0) = 0.;
-    identity()()()(1,1) = 1.;
-    identity()()()(1,2) = 0.;
-    identity()()()(2,0) = 0.;
-    identity()()()(2,1) = 0.;
-    identity()()()(2,2) = 1.;
-    
-    omega1()()()(0,0) = tmpcos - im*tmpsin;
-    omega1()()()(0,1) = 0.;
-    omega1()()()(0,2) = 0.;
-    omega1()()()(1,0) = 0.;
-    omega1()()()(1,1) = 1.;
-    omega1()()()(1,2) = 0.;
-    omega1()()()(2,0) = 0.;
-    omega1()()()(2,1) = 0.;
-    omega1()()()(2,2) = tmpcos + im*tmpsin;
-    
-    omega2()()()(0,0) = 0.;
-    omega2()()()(0,1) = 1.;
-    omega2()()()(0,2) = 0.;
-    omega2()()()(1,0) = 0.;
-    omega2()()()(1,1) = 0.;
-    omega2()()()(1,2) = 1.;
-    omega2()()()(2,0) = 1.;
-    omega2()()()(2,1) = 0.;
-    omega2()()()(2,2) = 0.;
-}
-    
-    
 }
 }
 }

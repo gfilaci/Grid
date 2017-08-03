91cce47c1e721a5b70d2db155c4d02d3eeaa897a
gfilaci <gianluca.filaci@gmail.com>
2017-08-03 17:06:29 +0100

    comparison with PRlgt is moved to another test file

--- tests/nspt/Test_wilsonpt.cc
+++ tests/nspt/Test_wilsonpt.cc
@@ -48,15 +48,17 @@ int main(int argc, char *argv[]) {
     
     GridParallelRNG pRNG(&Grid);
     pRNG.SeedFixedIntegers(std::vector<int>({45,12,81,9}));
-    RealD massstd=0;
-    PRealD mass;
-    zeroit(mass);
-    mass(2) = -1.;
-    mass(4) = -5.;
-    mass(6) = -10.;
+    
+    // masses and antiperiodicity
+    RealD massstd = 0.;
+    PRealD mass = zero;
+    WilsonImplParams Params;
+    Params.boundary_phases = {-1.,1.,1.,1};
+    
     
     // standard gauge field, set to identity
     QCD::LatticeGaugeField Ustd(&Grid);
+    zeroit(Ustd);
     parallel_for(int ss=0;ss<Grid.oSites();ss++){
         for (int alpha=0; alpha<Ns; alpha++) {
             for (int mu=0; mu<Nd; mu++) {
@@ -69,12 +71,12 @@ int main(int argc, char *argv[]) {
     // standard fermion fields, vectors in colour space
     QCD::LatticeFermion psistd(&Grid),psi2std(&Grid);
     // standard Wilson Dirac operator
-    WilsonFermion<QCD::WilsonImpl<vComplex,FundamentalRepresentation,CoeffReal>> Dwstd(Ustd,Grid,RBGrid,massstd);
+    WilsonFermion<QCD::WilsonImpl<vComplex,FundamentalRepresentation,CoeffReal>> Dwstd(Ustd,Grid,RBGrid,massstd,Params);
+    
     
     
     // perturbative gauge field, all matrices set to the identity (-> TBC are irrelevant)
     QCDpt::LatticeGaugeField U(&Grid);
-    PertVacuum(U);
     parallel_for(int ss=0;ss<Grid.oSites();ss++){
         for (int mu=0; mu<Nd; mu++) {
             for (int kk=0; kk<Np; kk++) {
@@ -84,17 +86,21 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    // perturbative fermion fields, matrices in colour space
+    // perturbative fermion fields, matrices in colour space, all matrices set to the identity (-> TBC are irrelevant)
     QCDpt::LatticeFermion psi(&Grid),psi2(&Grid),psicmp(&Grid);
-    // antiperiodic fermions
-    WilsonImplParams Params;
-    Params.boundary_phases = {-1.,1.,1.,1};
+    psi=zero;
+    parallel_for(int ss=0;ss<Grid.oSites();ss++){
+        for (int alpha=0; alpha<Ns; alpha++) {
+            for (int kk=0; kk<Np; kk++) {
+                for (int nn=0; nn<Nc; nn++) {
+                    (psi._odata[ss])()(alpha)(kk)(nn,nn) = 1.0;
+                }
+            }
+        }
+    }
     // perturbative Wilson Dirac operator
     WilsonFermion<QCDpt::PWilsonSmellImpl<vComplex,FundamentalRepresentation,CoeffReal>> Dw(U,Grid,RBGrid,mass,Params);
     
-    
-    // random field
-    random(pRNG,psi);
     
     // apply M
     Dw.M(psi,psi2);
@@ -111,7 +117,7 @@ int main(int argc, char *argv[]) {
                 }
             }
             
-            // apply M
+            // apply Mstd
             Dwstd.M(psistd,psi2std);
             
             // load psi2std onto ncol column of the smell matrix
@@ -131,73 +137,24 @@ int main(int argc, char *argv[]) {
         parallel_for(int ss=0;ss<Grid.oSites();ss++){
             for (int alpha=0; alpha<Ns; alpha++) {
                 for (int ord=0; ord<=kk; ord++) {
-                    (psi2._odata[ss])()(alpha)(kk) -= (psi2._odata[ss])()(alpha)(ord);
+                    (psi2._odata[ss])()(alpha)(kk) -= (psicmp._odata[ss])()(alpha)(ord);
                 }
+                (psi2._odata[ss])()(alpha)(kk) += 4.*(double)kk*(psi._odata[ss])()(alpha)(kk);
             }
         }
     }
     
-    cout<<Pnorm2(psi2)<<endl;
+    PComplex normvalue = Pnorm2(psi2);
     
+    bool ok=true;
+    for (int k=0; k<Np; k++)
+    if (norm(normvalue(k))>1e-12)
+    ok=false;
     
+    cout << GridLogMessage << "perturbative Wilson operator is consistent with standard Wilson operator : ";
+    if(ok) std::cout << "[OK] " << endl;
+    else cout << "[fail]" << endl << normvalue <<endl;
     
-    
-    /////////////////////////////////////////////
-    // TEST AGAINST PRlgt
-    /////////////////////////////////////////////
-    // note that gamma_peter = U gamma_franz U
-    // with
-    //  U = | sigma2    0   |
-    //      |   0    sigma2 |
-    /////////////////////////////////////////////
-    
-    iMatrix<Complex,Nc> AA,BB,CC;
-    Complex im(0.,1.);
-    
-    AA(0,0) = 0.0 + 9.0 * im;
-    AA(0,1) = 2.0 + 8.0 * im;
-    AA(0,2) = 4.0 + 7.0 * im;
-    AA(1,0) = 6.0 + 6.0 * im;
-    AA(1,1) = 8.0 + 5.0 * im;
-    AA(1,2) = 7.0 + 4.0 * im;
-    AA(2,0) = 5.0 + 3.0 * im;
-    AA(2,1) = 3.0 + 2.0 * im;
-    AA(2,2) = 1.0 + 1.0 * im;
-    
-    BB(0,0) = 0.1 + 5.9 * im;
-    BB(0,1) = 0.2 + 5.9 * im;
-    BB(0,2) = 0.3 + 5.8 * im;
-    BB(1,0) = 0.4 + 8.8 * im;
-    BB(1,1) = 0.5 + 8.7 * im;
-    BB(1,2) = 0.6 + 8.7 * im;
-    BB(2,0) = 0.7 + 3.6 * im;
-    BB(2,1) = 0.8 + 3.6 * im;
-    BB(2,2) = 0.9 + 3.5 * im;
-    
-    CC = AA*BB;
-    
-    // initialise psi
-    SpinPertColourMatrix tmpmat;
-    for (int x0=0; x0<4; x0++) {
-        for (int x1=0; x1<4; x1++) {
-            for (int x2=0; x2<4; x2++) {
-                for (int x3=0; x3<4; x3++) {
-                    zeroit(tmpmat);
-                    int x = x0*4*4*4 + x1*4*4 + x2*4 +x3;
-                    for (int alpha=0; alpha<Ns; alpha++) {
-                        for (int k=0; k<Np; k++) {
-                            tmpmat()(alpha)(k) = (double)x*AA + (double)alpha*BB + (double)k*CC;
-                        }
-                    }
-                    pokeSite(tmpmat,psi,std::vector<int>({x0,x1,x2,x3}));
-                }
-            }
-        }
-    }
-    
-    Dw.M(psi,psi2);
-//    cout<<psi2<<endl;
-    cout<<Dw.mass<<endl;
     Grid_finalize();
     return EXIT_SUCCESS;
     

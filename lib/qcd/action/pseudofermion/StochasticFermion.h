66dbe6618dcc48dc373bd32c581f2e9cd0c8fa54
gfilaci <gianluca.filaci@gmail.com>
2017-08-31 16:06:36 +0200

    now the action builds a vector of Wilson operators. deriv has the structure for the perturbative inversion

--- lib/qcd/action/pseudofermion/StochasticFermion.h
+++ lib/qcd/action/pseudofermion/StochasticFermion.h
@@ -34,33 +34,48 @@ namespace QCD {
 namespace QCDpt {
 
 ////////////////////////////////////////////////////////////////////////
-// Stochastic fermion action for any dop
+// Stochastic fermion action for Wilson fermions
 ////////////////////////////////////////////////////////////////////////
 template <class Impl>
 class StochasticFermionAction : public Action<typename Impl::GaugeField> {
  public:
   INHERIT_IMPL_TYPES(Impl);
-
- private:
-  FermionOperator<Impl> &FermOp;  // the basic operator
-  GridParallelRNG pRNG;
+  typedef typename Impl::SOimpl SOImpl;
+  typedef typename Impl::SOimpl::FermionField SOFermionField;
+  typedef typename Impl::SOimpl::Gimpl::Field SOGaugeField;
   
-//  OperatorFunction<FermionField> &DerivativeSolver;//$//
-
-  FermionField Phi;
+ private:
+  // vector of Wilson operators, one for each perturbative order
+  std::vector<WilsonFermion<SOImpl>> Dw;
+  SOGaugeField Uso, Uforce;
+  std::vector<SOFermionField> psi;
+  SOFermionField psitmp;
+  GridParallelRNG pRNG;
+  GridCartesian* grid;
+  // check this number... //$//
+  int Npf = Np - 2;
+  
 
  public:
+  
   /////////////////////////////////////////////////
   // Pass in required objects.
   /////////////////////////////////////////////////
-  StochasticFermionAction(FermionOperator<Impl> &Op, GridParallelRNG pRNG_/*,
-                                OperatorFunction<FermionField> &DS*/)
-      : FermOp(Op),
-        pRNG(pRNG_),
-        //DerivativeSolver(DS),
-        Phi(Op.FermionGrid()){};
-
-
+  StochasticFermionAction(GridParallelRNG pRNG_, GridCartesian* grid_, GridRedBlackCartesian* rbgrid_, PRealD mass_, WilsonImplParams Params_)
+      : pRNG(pRNG_),
+        Uso(grid_),
+        psitmp(grid_),
+        Uforce(grid_),
+        grid(grid_)
+        {
+            Dw.reserve(Npf);
+            psi.reserve(Npf);
+            for (int i=0; i<Npf; i++) {
+                Dw.push_back(WilsonFermion<SOImpl>(Uso,*grid_,*rbgrid_,mass_(i),Params_));
+                psi.push_back(SOFermionField(grid));
+            }
+        };
+  
   virtual std::string action_name(){return "StochasticFermionAction";}
 
   virtual std::string LogParameters(){
@@ -81,37 +96,49 @@ class StochasticFermionAction : public Action<typename Impl::GaugeField> {
   // dS/du = - Xi^dag  dM (M)^-1 Xi
   //////////////////////////////////////////////////////
   virtual void deriv(const GaugeField &U, GaugeField &dSdU) {
-      FermOp.ImportGauge(U);
+      for (int k=0; k<Npf; k++) {
+          Uso = peekPert(U,k);
+          Dw[k].ImportGauge(Uso);
+      }
       
-      //$// the noise should not be perturbative...
-      FermionField Xi(FermOp.FermionGrid());
-      FermionField invMXi(FermOp.FermionGrid());
-      decltype(peekPert(Xi,0)) tmp(FermOp.FermionGrid());
+      SOFermionField Xi(grid);
+      SOFermionField invMXi(grid);
       
       // Real part of Xi is gaussian with sigma = 1/sqrt(2),
       // same for imaginary part.
       // In this way < Xi^dag_a Xi_b > = delta_ab
-      gaussian(pRNG,tmp);
-      tmp *= M_SQRT1_2;
-      pokePert(Xi,tmp,0);
-      // (is it sure that gaussian generates real and imaginary parts independently?) //$//
+      gaussian(pRNG,Xi);
+      Xi *= M_SQRT1_2;
+      // (are we sure that 'gaussian' generates real and imaginary parts independently?) //$//
       
-      // compute invMXi = (M)^-1 Xi
-//      invMXi = zero;
-//      DerivativeSolver(M, Xi, invMXi);
+      psi[0] = Xi; // here I have to apply M0^-1...//$//
       
-      invMXi = Xi; //$// just to have something, waiting to be able to compute the inverse...
+      for (int n=1; n<Npf; n++) {
+          psi[n] = zero;
+          for (int j=0; j<n; j++) {
+              Dw[n-j].M(psi[j],psitmp);
+              psi[n] -= psitmp;
+          }
+          // apply M0^-1 to psi[n] //$//
+      }
       
-      // compute force
-      FermOp.MDeriv(dSdU, Xi, invMXi, DaggerNo);
-      //$// remember algebra projection
+      for (int n=0; n<Npf; n++) {
+          Uforce = zero;
+          for (int j=0; j<=n; j++) {
+              Dw[n-j].MDeriv(Uso, Xi, psi[j], DaggerNo);
+              Uforce += Uso;
+          }
+          //$// remember algebra projection
+          //$// remember to set dSdU to zero and shift the orders
+          pokePert(dSdU,Uforce,n);
+      }
       
       // ********************************** //
       // CHECK CONVENTIONS AND OVERALL SIGN //
       // ********************************** //
       // Our conventions really make this UdSdU; We do not differentiate wrt Udag here.
       // So must take dSdU - adj(dSdU) and left multiply by mom to get dS/dt.
-      // not taking here the traceless antihermitian component
+      // not taking here the traceless antihermitian component*/
   };
 };
 }

8ddb361e31b56af1970f93ac68ae894032fa979d
gfilaci <gianluca.filaci@gmail.com>
2017-09-20 11:37:01 +0100

    fixed mpi problem

--- lib/communicator/Communicator_mpi3_leader.cc
+++ lib/communicator/Communicator_mpi3_leader.cc
@@ -830,6 +830,13 @@ CartesianCommunicator::CartesianCommunicator(const std::vector<int> &processors)
   MPI_Cart_coords(communicator,_processor,_ndimension,&_processor_coor[0]);
 };
 
+CartesianCommunicator::~CartesianCommunicator(){
+    if (communicator && !MPI::Is_finalized()){
+        MPI_Comm_free(&communicator);
+    }
+}
+
+
 void CartesianCommunicator::GlobalSum(uint32_t &u){
   int ierr=MPI_Allreduce(MPI_IN_PLACE,&u,1,MPI_UINT32_T,MPI_SUM,communicator);
   assert(ierr==0);

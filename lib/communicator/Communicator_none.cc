9190bbc67eefa140e4d3ac1b1f8b0799ba1dcedd
gfilaci <gianluca.filaci@gmail.com>
2017-10-02 09:47:29 +0100

    destructor for none comm

--- lib/communicator/Communicator_none.cc
+++ lib/communicator/Communicator_none.cc
@@ -53,6 +53,8 @@ CartesianCommunicator::CartesianCommunicator(const std::vector<int> &processors)
   }
 }
 
+CartesianCommunicator::~CartesianCommunicator(){}
+
 void CartesianCommunicator::GlobalSum(float &){}
 void CartesianCommunicator::GlobalSumVector(float *,int N){}
 void CartesianCommunicator::GlobalSum(double &){}

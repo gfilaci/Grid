c8db3b3dade0e6f330dba94450199a4e1e537402
gfilaci <gianluca.filaci@gmail.com>
2018-02-21 11:41:17 +0000

    new default Np and Nf

--- tests/nspt/Test_runstaggered.cc
+++ tests/nspt/Test_runstaggered.cc
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     GaugeLevel.push_back(&GaugeAction);
     
     RealD mass = 0.;
-    int Nf = 16;
+    int Nf = 2;
     WilsonImplParams Params;
     Params.boundary_phases = {-1.,1.,1.,1};
     
